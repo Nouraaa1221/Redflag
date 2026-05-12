71cfe43804593eddd25c8b716400e66d65ffc271
Nouraaa1221 <nouradiaby1112@gmail.com>
2026-05-12 21:29:44 +0200

    Update Transaction.java

--- java/Transaction.java
+++ java/Transaction.java
@@ -1,1 +1,27 @@
+public class Transaction {
+    private String id;
+    private double montant;
+    private String pays;
+    private int heure;
+    private String type;
 
+    public Transaction(String id, double montant, String pays, int heure, String type) {
+        this.id = id;
+        this.montant = montant;
+        this.pays = pays;
+        this.heure = heure;
+        this.type = type;
+    }
+
+    public String getId() { return id; }
+    public double getMontant() { return montant; }
+    public String getPays() { return pays; }
+    public int getHeure() { return heure; }
+    public String getType() { return type; }
+
+    @Override
+    public String toString() {
+        return "Transaction{id='" + id + "', montant=" + montant +
+               ", pays='" + pays + "', heure=" + heure + "}";
+    }
+}
